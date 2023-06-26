c19d247700bb6e9b65cc79b8d9d1bcb78b622c8d
emreustundag1618 <emre.ustundag.1.618@gmail.com>
2023-06-26 15:58:39 +0300

    update the server URL

--- client/script.js
+++ client/script.js
@@ -87,7 +87,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv)
 
-  const response = await fetch('http://localhost:3000/', {
+  const response = await fetch('https://first-chatgpt-app.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
